1269fa5ef293529f78c9e2444326e3b00b10b539
omegeangel <i.bgchggu@gmail.com>
2025-07-18 19:31:23 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
 import dotenv from 'dotenv';
 dotenv.config();
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { 
   Globe, 
